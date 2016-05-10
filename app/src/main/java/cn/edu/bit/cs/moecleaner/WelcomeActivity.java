250ab1b69b9761535b0c828f08ef1e32c2c4d92b
entalent <1512503696@qq.com>
2016-05-10 08:11:14 +0800

    final commit of 1.0.0 version

--- app/src/main/java/cn/edu/bit/cs/moecleaner/WelcomeActivity.java
+++ app/src/main/java/cn/edu/bit/cs/moecleaner/WelcomeActivity.java
@@ -38,7 +38,7 @@ public class WelcomeActivity extends AppCompatActivity {
                 public void run() {
                     finish();
                 }
-            }, 3000);
+            }, 2500);
         }
     }
 
